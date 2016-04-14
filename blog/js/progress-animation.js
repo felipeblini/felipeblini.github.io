a83fe871f3e6a1f62175173f565c9bd636f06932
Felipe blini <Felipe.blini@gmail.com>
2016-04-14 03:08:22 -0300

    modified menu and added about page

---
+++ blog/js/progress-animation.js
@@ -0,0 +1,22 @@
+(function () {
+    $(document).ready(function() {
+        fillBars()
+    });
+    
+    $(window).scroll(function() {
+        fillBars()
+    });
+    
+    function fillBars() {
+        console.log('size', $(".progressanim").length);
+        
+        $(".progressanim").each(function() {
+            var pos = $(this).offset().top;
+            var winTop = $(window).scrollTop();
+            
+            if (pos < winTop + 750) {
+                $(this).css('width', $(this).data('percent') + '%');
+            }
+        })
+    }
+})();
